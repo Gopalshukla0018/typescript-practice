2af1cd3a0a93fadbe66c1df99a11566e3bd1b26e
Gopal Shukla <gopalshukla0018@gmail.com>
2026-03-18 12:25:31 +0530

    fns

--- 04/01_functions.ts
+++ 04/01_functions.ts
@@ -14,3 +14,28 @@ function greet(name: string): void {
 }
 
 greet("Gopal");
+
+// let combineFn:Function;
+
+// combineFn=()=>{
+//     console.log("Gopal")
+// }   // invalid
+
+// combineFn=add;
+// // combineFn=greet;
+// console.log(combineFn(1,4));
+
+// Goos practice
+
+let combineFn: (a: number, b: number) => number;
+combineFn = add;
+console.log(combineFn(100, 29));
+
+function handleSum(num1: number, num2: number, cb: (n: number) => void) {
+  const sum = num1 + num2;
+  cb(sum);
+}
+
+handleSum(10, 20, (sum:number ) => {
+  console.log(sum);
+});
